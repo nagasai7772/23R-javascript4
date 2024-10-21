85bd36beccfb556485b17793f6642c9e9e12d6be
nagasai7772 <nagunagasai@gmail.com>
2024-10-21 15:36:34 +0530

    string methods

---
+++ index.js
@@ -0,0 +1,22 @@
+//Join method------
+
+var fruits=['Apple','Banana','Orange'];
+console.log(fruits.join(" , "));  
+var numbers=[1,2,3,4,5];
+console.log(numbers.join("-"));
+var chars=['H','E','L','L','O'];
+console.log(chars.join(""));
+//slice method-------
+var text='Javascript is awesome';
+console.log(text.slice(0,10));
+var sentence= 'I love learning JavaScript!';
+console.log(sentence.slice(16,26));
+var text='Frontend Development!';
+console.log(text.slice(-12,-1));
+//split method---------
+var date='2024-10-21';
+console.log(date.split("-"));
+var text2='I love Javascript Programming';
+console.log(text2.split(" ").slice(0,2));
+let url='https://www.example.com/path/page.html';
+console.log(url.split("/"));
